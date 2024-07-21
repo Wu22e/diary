d0f2cbb623c63063e54513dd264c1bcdfc02e27f
gulb1602 <gulb1602@gmail.com>
2024-07-21 16:58:29 +0900

    use gin framework

--- go-backend-inf/crud_server/init/cmd/cmd.go
+++ go-backend-inf/crud_server/init/cmd/cmd.go
@@ -2,18 +2,24 @@ package cmd
 
 import (
 	"crud_server/config"
+	"crud_server/network"
 	"fmt"
 )
 
 type Cmd struct {
-	config *config.Config
+	config  *config.Config
+	network *network.Network
 }
 
 func NewCmd(filePath string) *Cmd {
 	c := &Cmd{
-		config: config.NewConfig(filePath),
+		config:  config.NewConfig(filePath),
+		network: network.NewNetwork(),
 	}
 
+	c.network.ServerStart(c.config.Server.Port)
+
+	// not reached until channel(gin network) close
 	fmt.Println(c.config.Server.Port)
 
 	return c
--- go-backend-inf/crud_server/network/root.go
+++ go-backend-inf/crud_server/network/root.go
@@ -1,1 +1,20 @@
 package network
+
+import "github.com/gin-gonic/gin"
+
+type Network struct {
+	engine *gin.Engine
+}
+
+// Among the network frameworks mux, echo, and gin, we will use gin, which is the most widely used.
+func NewNetwork() *Network {
+	r := &Network{
+		engine: gin.New(),
+	}
+
+	return r
+}
+
+func (n *Network) ServerStart(port string) error {
+	return n.engine.Run(port)
+}
