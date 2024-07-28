391051606c50f8254c9a6763a22923c574a1ab1f
gulb1602 <gulb1602@gmail.com>
2024-07-28 19:45:15 +0900

    implement apps for 3 tier

--- go-backend-inf/rpc_server/main.go
+++ go-backend-inf/rpc_server/main.go
@@ -2,6 +2,7 @@ package main
 
 import (
 	"flag"
+	"rpc_server/cmd"
 	"rpc_server/config"
 )
 
@@ -9,5 +10,7 @@ var configFlag = flag.String("config", "./config.toml", "config path")
 
 func main() {
 	flag.Parse()
-	config.NewConfig(*configFlag)
+	cfg := config.NewConfig(*configFlag)
+
+	cmd.NewApp(cfg)
 }
--- go-backend-inf/rpc_server/network/router.go
+++ go-backend-inf/rpc_server/network/router.go
@@ -1,1 +1,18 @@
 package network
+
+import (
+	"rpc_server/config"
+	"rpc_server/service"
+)
+
+type Network struct {
+	cfg *config.Config
+
+	service *service.Service
+}
+
+func NewNetwork(cfg *config.Config, service *service.Service) (*Network, error) {
+	n := &Network{cfg: cfg, service: service}
+
+	return n, nil
+}
