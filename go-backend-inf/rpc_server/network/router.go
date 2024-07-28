15df571e9fcc9c204243d720a990fdbb7d360e79
gulb1602 <gulb1602@gmail.com>
2024-07-28 20:00:11 +0900

    start server using gin framework

--- go-backend-inf/rpc_server/network/router.go
+++ go-backend-inf/rpc_server/network/router.go
@@ -3,16 +3,24 @@ package network
 import (
 	"rpc_server/config"
 	"rpc_server/service"
+
+	"github.com/gin-gonic/gin"
 )
 
 type Network struct {
 	cfg *config.Config
 
 	service *service.Service
+
+	engine *gin.Engine
 }
 
 func NewNetwork(cfg *config.Config, service *service.Service) (*Network, error) {
-	n := &Network{cfg: cfg, service: service}
+	n := &Network{cfg: cfg, service: service, engine: gin.New()}
 
 	return n, nil
+}
+
+func (n *Network) StartServer() {
+	n.engine.Run(":8080")
 }
