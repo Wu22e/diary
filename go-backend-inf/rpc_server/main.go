217f89072d7ecd2197fd9e67966d6fc3155efabd
gulb1602 <gulb1602@gmail.com>
2024-07-29 00:10:07 +0900

    connect grpc & http server

--- go-backend-inf/rpc_server/main.go
+++ go-backend-inf/rpc_server/main.go
@@ -4,6 +4,8 @@ import (
 	"flag"
 	"rpc_server/cmd"
 	"rpc_server/config"
+	"rpc_server/gRPC/server"
+	"time"
 )
 
 var configFlag = flag.String("config", "./config.toml", "config path")
@@ -12,5 +14,10 @@ func main() {
 	flag.Parse()
 	cfg := config.NewConfig(*configFlag)
 
-	cmd.NewApp(cfg)
+	if err := server.NewGRPCServer(cfg); err != nil {
+		panic(err)
+	} else {
+		time.Sleep(1e9)
+		cmd.NewApp(cfg)
+	}
 }
--- go-backend-inf/rpc_server/network/router.go
+++ go-backend-inf/rpc_server/network/router.go
@@ -2,6 +2,7 @@ package network
 
 import (
 	"rpc_server/config"
+	"rpc_server/gRPC/client"
 	"rpc_server/service"
 
 	"github.com/gin-gonic/gin"
@@ -10,13 +11,14 @@ import (
 type Network struct {
 	cfg *config.Config
 
-	service *service.Service
+	service    *service.Service
+	gRPCClient *client.GRPCClient
 
 	engine *gin.Engine
 }
 
-func NewNetwork(cfg *config.Config, service *service.Service) (*Network, error) {
-	n := &Network{cfg: cfg, service: service, engine: gin.New()}
+func NewNetwork(cfg *config.Config, service *service.Service, gRPCClient *client.GRPCClient) (*Network, error) {
+	n := &Network{cfg: cfg, service: service, engine: gin.New(), gRPCClient: gRPCClient}
 
 	return n, nil
 }
